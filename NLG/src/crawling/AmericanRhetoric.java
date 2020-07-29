7394e86de46f7c57821567b654d6a67a37bbb31d
j.buelling@gmail.com <j.buelling@gmail.com>
2020-07-29 11:43:04 +0200

    restructured

--- NLG/src/crawling/AmericanRhetoric.java
+++ NLG/src/crawling/AmericanRhetoric.java
@@ -1,3 +1,4 @@
+package crawling;
  import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
--- NLG/src/crawling/FactBase.java
+++ NLG/src/crawling/FactBase.java
@@ -1,12 +1,11 @@
-import java.io.BufferedWriter;
+package crawling;
+ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.net.SocketTimeoutException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
