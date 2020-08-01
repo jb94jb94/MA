2a8090c9468009a118b3a86c90600d237600a2d6
j.buelling@gmail.com <j.buelling@gmail.com>
2020-08-01 13:58:01 +0200

    simple RNN for text generation

---
+++ NLG/src/model/tf_tut.py
@@ -0,0 +1,163 @@
+'''
+Created on 12.03.2020
+
+@author: jbuel
+'''
+
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+import sys
+import json
+import re
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+root = os.path.dirname(os.path.dirname(os.path.abspath(os.path.curdir)))
+text = ""
+for i in range(1,474):
+        try:
+            fileLoc = root+"/AmRhet/text/"+str(i)+".txt"
+            file= open(fileLoc,"r")
+            a = str(json.load(file)['text'])
+            a = re.sub(r'[^\w\s]','',a)
+            file.close()
+            text+=a
+        except:
+            print(sys.exc_info()[0])
+print(text[0:500])
+print(len(text))
+
+vocab =sorted(set(text))
+
+char2idx = {u:i for i,u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+seq_length = 200
+examples_per_epoch=len(text)
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length+1,drop_remainder=True)
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+ 
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in  dataset.take(1):
+    print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+    for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+        print("Step {:4d}".format(i))
+        print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+        print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+
+BATCH_SIZE=64
+BUFFER_SIZE = 10000
+
+for input_example, target_example in  dataset.take(1):
+  print ('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+  print ('Target data:', repr(''.join(idx2char[target_example.numpy()])))
+  
+dataset= dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE,drop_remainder=True)
+
+#model building
+
+vocab_size=len(vocab)
+embedding_dim= 256
+rnn_units=1024
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    tf.keras.layers.GRU(rnn_units,
+                        return_sequences=True,
+                        stateful=True,
+                        recurrent_initializer='glorot_uniform'),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(
+  vocab_size = len(vocab),
+  embedding_dim=embedding_dim,
+  rnn_units=rnn_units,
+  batch_size=BATCH_SIZE)
+
+
+for input_example_batch, target_example_batch in dataset.take(1):
+  example_batch_predictions = model(input_example_batch)
+  print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+  sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+  sampled_indices = tf.squeeze(sampled_indices,axis=-1).numpy()
+  print(sampled_indices)
+  print("Input: \n", repr("".join(idx2char[input_example_batch[0]])))
+  print()
+  print("Next Char Predictions: \n", repr("".join(idx2char[sampled_indices ])))
+
+def loss(labels, logits):
+  return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+example_batch_loss  = loss(target_example_batch, example_batch_predictions)
+print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
+print("scalar_loss:      ", example_batch_loss.numpy().mean())
+
+model.compile(optimizer='adam', loss=loss)
+
+# Directory where the checkpoints will be saved
+
+checkpoint_dir = './training_checkpoints'
+
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+EPOCHS=30
+history =model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+
+#tf.train.latest_checkpoint(checkpoint_dir)
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+#load current weights: tf.train.latest_checkpoint(checkpoint_dir)
+#best so far: root+"/src/model/training_checkpoints/ckpt_obama"
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+  num_generate = 500
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+  
+  text_generated = []
+  temperature = 0.5
+
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the character returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted character as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+print(generate_text(model,start_string=u"racism"))
